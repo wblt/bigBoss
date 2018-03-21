309f4365c42f0b30593957f668873eaaa1c99790
wyChris <281269704@qq.com>
2018-03-21 15:39:29 +0800

    123

--- pages/customer/dynamic/dynamic.js
+++ pages/customer/dynamic/dynamic.js
@@ -5,7 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    hiddenmodalput: true,
+        //可以通过hidden是否掩藏弹出框的属性，来指定那个弹出框 
+  },
+
+  //点击按钮痰喘指定的hiddenmodalput弹出框  
+  adddynamic: function () {
+    this.setData({
+      hiddenmodalput: !this.data.hiddenmodalput
+    })
+  },
+  //取消按钮  
+  cancel: function () {
+    this.setData({
+      hiddenmodalput: true
+    });
+  },
+  //确认  
+  confirm: function () {
+    this.setData({
+      hiddenmodalput: true
+    })
   },
 
   /**
