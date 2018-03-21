62ac1d735ca65b11f4f612c8ad484df589eacf57
wblt <940422068@qq.com>
2018-03-21 11:15:50 +0800

    d

--- pages/more/more.js
+++ pages/more/more.js
@@ -1,23 +1,17 @@
-// pages/more/more.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+var app = getApp()
+Page({
   data: {
-
+    userListInfo: [{
+      icon: '../img/guanjianzi.png',
+      text: '行业关键字'
+    }, {
+      icon: '../img/kehuwangzan.png',
+      text: '行业客户网站'
+    }]
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
   }
 })
